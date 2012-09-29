183ee3f1b1a235b94be2d94f653d8e212e6dd32b
Rik van der Kooij <rikvanderkooij@gmail.com>
2012-09-29 17:16:25 +0200

    concurrency ass removed the thrown exptions in CGL and added little readme

--- concurrency/cm_assignment/src/data_structures/implementation/CoarseGrainedList.java
+++ concurrency/cm_assignment/src/data_structures/implementation/CoarseGrainedList.java
@@ -5,14 +5,17 @@ import data_structures.Sorted;
 public class CoarseGrainedList<T extends Comparable<T>> implements Sorted<T> {
 
 	public void add(T t) {
-		throw new UnsupportedOperationException();
+		//throw new UnsupportedOperationException();
+        return;
 	}
 
 	public void remove(T t) {
-		throw new UnsupportedOperationException();
+		//throw new UnsupportedOperationException();
+        return;
 	}
 
 	public String toString() {
-		throw new UnsupportedOperationException();
+		//throw new UnsupportedOperationException();
+        return "Bla";
 	}
 }
