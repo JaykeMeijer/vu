5f718622c6d49f8bea8cff241a8e0bec76287e1a
Rik van der Kooij <rikvanderkooij@gmail.com>
2012-09-30 13:24:48 +0200

    concurency ass CGL added from book page 200/201

--- concurrency/cm_assignment/src/data_structures/implementation/CoarseGrainedList.java
+++ concurrency/cm_assignment/src/data_structures/implementation/CoarseGrainedList.java
@@ -1,21 +1,73 @@
 package data_structures.implementation;
 
 import data_structures.Sorted;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class CoarseGrainedList<T extends Comparable<T>> implements Sorted<T> {
 
+    private Node<T> head;
+    private ReentrantLock lock = new ReentrantLock();
+
+    public CoarseGrainedList() {
+        head = new Node<T>(Integer.MIN_VALUE);
+        head.next = new Node<T>(Integer.MAX_VALUE);
+    }
+
 	public void add(T t) {
-		//throw new UnsupportedOperationException();
+        Node pred, curr;
+        int key = t.hashCode();
+        lock.lock();
+        try {
+            pred = head;
+            curr = head.next;
+            while(curr.key < key) {
+                pred = curr;
+                curr = curr.next;
+            }
+            Node<T> node = new Node<T>(t, key);
+            pred.next = node;
+            node.next = curr;
+        } finally {
+            lock.unlock();
+        }
         return;
 	}
 
 	public void remove(T t) {
-		//throw new UnsupportedOperationException();
+        Node pred, curr;
+        int key = t.hashCode();
+        lock.lock();
+        try {
+            pred = head;
+            curr = head.next;
+            while(curr.key < key) {
+                pred = curr;
+                curr = curr.next;
+            }
+            if(curr.key == key)
+                pred.next = curr.next;
+        } finally {
+            lock.unlock();
+        }
         return;
 	}
 
 	public String toString() {
-		//throw new UnsupportedOperationException();
-        return "Bla";
+        return "";
 	}
+
+    private static class Node<T> {
+        public T item = null;
+        public final int key;
+        public CoarseGrainedList.Node next;
+
+        Node(int k) {
+            key = k;
+        }
+
+        Node(T i, int k) {
+            item = i; 
+            key = k; 
+        }
+    }
 }
