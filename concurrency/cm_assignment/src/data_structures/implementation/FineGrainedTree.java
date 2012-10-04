8fbe36abd4fd2da3c93ca6c0f7f13e903efe74c9
icyrizard <rich.riz@gmail.com>
2012-10-04 11:42:15 +0200

    Merge and started on finegrainedtree

--- concurrency/cm_assignment/src/data_structures/implementation/FineGrainedTree.java
+++ concurrency/cm_assignment/src/data_structures/implementation/FineGrainedTree.java
@@ -5,14 +5,60 @@ import data_structures.Sorted;
 public class FineGrainedTree<T extends Comparable<T>> implements Sorted<T> {
 
 	public void add(T t) {
-		throw new UnsupportedOperationException();
+		Node pred, curr;
+
+		
 	}
 
 	public void remove(T t) {
-		throw new UnsupportedOperationException();
+		
 	}
 
 	public String toString() {
-		throw new UnsupportedOperationException();
+		
 	}
+
+   	abstract class Node {
+   		private ReentrantLock lock = new ReentrantLock();
+        T item = null;
+        Node left = null;
+        Node right = null;
+
+        protected abstract int compareTo(T t);
+        protected abstract lock();
+        protected abstract unlock();
+    }
+
+    /* sentinal head node */
+    class HeadNode extends Node {
+        protected int compareTo(T t) {
+            return -1;
+        }
+    }
+
+    /* sentinal tail node */
+    class TailNode extends Node {
+        protected int compareTo(T t) {
+            return 1;
+        }
+    }
+
+    /* normal list node */
+    class TreeNode extends Node {
+        TreeNode(T t) {
+            item = t;
+        }
+
+        public lock(){
+        	lock.lock();
+        }
+
+        public lock(){
+        	lock.unlock();
+        }
+
+        protected int compareTo(T t) {
+            return item.compareTo(t);
+        }
+    }
 }
