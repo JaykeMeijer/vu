71164e04e0e665ed58f7e2601fec56c8a5ea718c
icyrizard <rich.riz@gmail.com>
2012-10-06 16:50:37 +0200

    some minor changes, not to logic

--- concurrency/cm_assignment/src/data_structures/implementation/LockFreeTree.java
+++ concurrency/cm_assignment/src/data_structures/implementation/LockFreeTree.java
@@ -5,14 +5,14 @@ import data_structures.Sorted;
 public class LockFreeTree<T extends Comparable<T>> implements Sorted<T> {
 
 	public void add(T t) {
-		throw new UnsupportedOperationException();
+		
 	}
 
 	public void remove(T t) {
-		throw new UnsupportedOperationException();
+		
 	}
 
 	public String toString() {
-		throw new UnsupportedOperationException();
+		
 	}
 }
