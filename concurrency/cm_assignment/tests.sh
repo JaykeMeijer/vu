1eb4be1c3756816e9e20fd8cf3f5d2c9a5a3a88d
Rik van der Kooij <rikvanderkooij@gmail.com>
2012-10-10 17:23:51 +0200

    report worked on cgl

--- concurrency/cm_assignment/tests.sh
+++ concurrency/cm_assignment/tests.sh
@@ -2,9 +2,11 @@
 
 # dit is gwn wat random shit
 DATA=( cgl cgt fgl fgt lfl lft )
-THREADS=12
-WORKLOAD=( 0 10 20 30 40 50 )
-NUMEL=( 10000 20000 30000 40000 50000 60000 70000 80000 90000 100000)
+THREADS=2
+WORKLOAD=( 10 )
+#10 20 30 40 50 )
+NUMEL=( 10000 )
+#20000 30000 40000 50000 60000 70000 80000 90000 100000)
 
 echo "<data_structure> <nrThreads> <nrItems> <workTime> <runTime>" > file.txt
 
