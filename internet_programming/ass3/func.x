77ad8f082cb690b71140f5128e6e83d1d8892502
Rik van der Kooij <rikvanderkooij@gmail.com>
2012-10-15 02:10:17 +0200

    rpc ass

---
+++ internet_programming/ass3/func.x
@@ -0,0 +1,10 @@
+
+
+program PAPER_SERVER
+{
+    version PAPER_VERSION
+    {
+        int ADD_PAPER() = 1;
+
+    } = 1;
+}
--- internet_programming/ass3/paperclient.c
+++ internet_programming/ass3/paperclient.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "headers/writen_error.h"
 
-void show_options()
+void show_options(const char *str)
 {
-    fputs("INFO\n", stdout);
+    printf("usage: %s <hostname> <action> <arguments>\n", str);
+    printf("actions:\n"); 
+    printf("store article:    add <Author Name> <Paper Title> <file>\n");
+    printf("remove article:   remove <number>\n");
+    printf("list articles:    list\n");
+    printf("display info of an article:  info <number>\n");
+    printf("fetch an article: fetch <number>\n\n");
+    printf("example add:    %s localhost remove 4\n", str);
+    exit(-1);
 }
 
 
@@ -11,10 +21,8 @@ int main(int argc, const char *argv[])
     int i;
 
     if(argc < 2)
-        show_options();
+        show_options(argv[0]);
     else {
-        for(i = 1; i < argc; i++)
-            printf("%s\n", argv[i]);
 
 
     }
