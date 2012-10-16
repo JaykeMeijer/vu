09dfd709c80e2f249f0117c7130c5cb9f27bcd6a
Rik van der Kooij <rikvanderkooij@gmail.com>
2012-10-16 12:02:22 +0200

    sunrpc update

--- internet_programming/ass3/paperclient.c
+++ internet_programming/ass3/paperclient.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "add.h"
 
 void show_options(const char *str)
@@ -9,28 +10,85 @@ void show_options(const char *str)
     printf("store article:    add <Author Name> <Paper Title> <file>\n");
     printf("remove article:   remove <number>\n");
     printf("list articles:    list\n");
-    printf("display info of an article:  info <number>\n");
+    printf("article info:     info <number>\n");
     printf("fetch an article: fetch <number>\n\n");
-    printf("example add:    %s localhost remove 4\n", str);
+    printf("example remove:   %s localhost remove 4\n", str);
     exit(-1);
 }
+
+void add(const char *arg[])
+{
+    FILE *f;
+    long size;
+
+    printf("adding paper\n");
+
+    f = fopen(arg[2], "rb");
+    if(f == NULL) {
+        printf("File %s not found", arg[2]);
+        exit(-1);
+    }
+    /* get size of file */
+    if(!fseek(f, 0, SEEK_END)) {
+        perror("fseek failed");
+        exit(-1);
+    }
+    size = ftell(f);
+    if(size < 0) {
+        perror("ftell failed");
+        exit(-1);
+    }
+    rewind(f);
+    fclose(f);
+
+    printf("%ld", size);
+
+    return;
+}
+void remove_paper() { return; }
+void list() { return; }
+void info() { return; }
+void fetch() { return; }
 
 
 int main(int argc, const char *argv[])
 {
     CLIENT *cl;
-    param in;
-    add_out *out;
+    //param in;
+    //add_out *out;
     
-    if(argc < 2)
+    if(argc < 3)
         show_options(argv[0]);
 
     cl = clnt_create(argv[1], PAPER_SERVER, PAPER_VERSION, "tcp");
-    in.name = "Jan jansen"; 
-    in.title = "Test paper"; 
-    in.next = 0;
+    if(cl == NULL) {
+        perror("No RPC server found");
+        exit(-1);
+    }
 
-    out = add_1(&in, cl);
-    printf("out.arg: %d\n", (int)*out);
+    if(!strcmp(argv[2], "add") && argc >= 6)
+        add(argv+3);
+    else if(!strcmp(argv[2], "remove") && argc >= 4)
+        remove_paper();
+    else if(!strcmp(argv[2], "list"))
+        list();
+    else if(!strcmp(argv[2], "info"))
+        info();
+    else if(!strcmp(argv[2], "fetch") && argc >= 4)
+        fetch();
+    else
+        show_options(argv[0]);
+
+
+    //in.name = argv[3];
+    //in.title = argv[4]; 
+    //in.next = 0;
+    //out = add_1(&in, cl);
+    //printf("out.arg: %d\n", (int)*out);
+
     return 0;
 }
+
+
+
+
--- internet_programming/ass3/paperserver.c
+++ internet_programming/ass3/paperserver.c
@@ -1,8 +1,6 @@
 #include "add.h"
 
 
-
-
 add_out *add_1_svc(param *in, struct svc_req *rqstp)
 {
     static add_out out;
