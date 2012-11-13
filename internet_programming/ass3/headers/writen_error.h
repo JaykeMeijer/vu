64e915ad13441c410f1f3f36d890363100b999f0
Rik van der Kooij <rikvanderkooij@gmail.com>
2012-11-13 18:42:08 +0100

    writen error files deleted

--- internet_programming/ass3/headers/writen_error.h
+++
@@ -1,8 +0,0 @@
-/* error exit function 
- * prints error string parameter given 
- * and perror before exiting on a 
- * negative integer */
-void error_handling(int error, char *str);
-
-/* writen function from Stevens book page 78 */
-ssize_t writen(int fd, const void *vptr, size_t n);
